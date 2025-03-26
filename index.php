58c81cf425078bad2944dfc7e47e883996a60518
Aku6727 <antkutovoy@gmail.com>
2025-03-27 09:15:45 +1300

    Added a custom font. Made a index file. Started testing out bootstrap

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voidtech</title>
+</head>
+<body>
+    <div class = 'header'>
+        <h1>Voidtech</h1>   
+    </div>
+    
+</body>
+</html>
