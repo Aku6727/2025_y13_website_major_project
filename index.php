240dd5d22892ae9215d7c1c64407fb9bbf6549f6
Aku6727 <antkutovoy@gmail.com>
2025-03-29 09:24:15 +1300

    Upload some placeholder images. Started creating the index. Made navbar include file and the hero image

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Voidtech</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
 </head>
-<body>
-    <div class = 'header'>
-        <h1>Voidtech</h1>   
+  <body>
+    <!-- Include the navbar -->
+    <?php
+      include("navbar.php")
+    ?>
+    <!-- Add the main image with slogan -->
+    <div? class = "hero_index">
+      <!-- hero image -->
+      <img src="images/gaming_mouse.jpg" alt="Mouse" class="background_index_image">
+      <!-- Slogan -->
+      <h1 class = "hero_text">Innovation elevated</h1>
     </div>
+
+  
     
-</body>
+  </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar_custom " data-bs-theme="dark" navbar bg-dark>
+  <div class="container-fluid">
+    <a class="navbar-brand company_name" href="index.php" >
+        Voidtech
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="browse.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Browse
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="log_in.php">Support</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-disabled="false">Log in/Sign up</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
