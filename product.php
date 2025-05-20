395f57fbccf2d48bd2024b7140eb1f78fbc21987
Aku6727 <antkutovoy@gmail.com>
2025-05-21 11:10:30 +1200

    Fixed any other query issues. Need to style results

--- product.php
+++ product.php
@@ -18,41 +18,35 @@
     <?php 
         $item_chosen = isset($_GET['part_id']) ? $_GET['part_id'] : null;
         if (!is_null($item_chosen)){  
-            $item_sql = "SELECT products.id, products.prod_name, products.type_id, products.price, manufacturer.manufacturer_name, products.weight, connection.connection_type, products.image FROM products 
+            $item_sql = "SELECT products.id, products.prod_name, product_type.product_type, products.price, manufacturer.manufacturer_name, products.weight, connection.connection_type, products.image FROM products 
             INNER JOIN manufacturer ON products.manufacturer_id = manufacturer.manufacturer_id 
+            INNER JOIN product_type ON products.type_id = product_type.type_id
             INNER JOIN connection ON products.connection_id = connection.connection_id WHERE products.id = $item_chosen";
             $item_query = mysqli_query($dbconnect, $item_sql);
             $item_rs = mysqli_fetch_assoc($item_query);
             ?>
             
-            <div class = 'product_container'>
 
-            <!-- add breadcrumbs -->
-            <div class='breadcrumbs'>
-            <p><a href='index.php'>Home</a> / <a href='browse.php'>Browse</a> / <a href='product.php?part_id=<?php echo $item_chosen; ?>'><?php echo $item_rs['part_name']; ?></a></p>
-            </div>
 
             <?php
             echo "<div class='product_content'>";
             // Check if there are any results
             if ($item_query && mysqli_num_rows($item_query) > 0) {
                 // If there are results, display the photo and specs
-                echo "<div class='product_image'><img src='Photos/" . $item_rs['photo'] . "' alt='item image'></div>";
+                echo "<div class='product_image'><img src='images/" . $item_rs['image'] . "' alt='item image'></div>";
                 // Display a blurb and the specs
                 echo "<div class='product_description'>
-                        <div class='product_title'><h1>" . $item_rs['part_name'] . "</h1></div>
+                        <div class='product_title'><h1>" . $item_rs['prod_name'] . "</h1></div>
                         <div class='product_specs'>
                             <p>All innovixus produts are made to be at the avsolute pinacle of performance. <br>
                                 We are confident that all our products are the absolutely the best in the world 
                                 and therefore give you the following guarntee: <br> If you are able to find a product that can outperform
                                 our one we will mtach it's price</p>
-                            <p>Type: " . $item_rs['type_name'] . "</p>
+                            <p>Type: " . $item_rs['product_type'] . "</p>
                             <p>Price: $" . $item_rs['price'] . "</p>
-                            <p>Processor: " . $item_rs['processor'] . "</p>
-                            <p>Memory amount: " . $item_rs['memory'] . "GB</p>    
-                            <p>Cooling: " . $item_rs['cooling_type'] . " Technology</p>   
-                            <p>RGB: " . $item_rs['rgb'] . "</p>
-                            <p>Bios Type: " . $item_rs['bios_name'] . "</p>
+                            <p>Manufacturer: " . $item_rs['manufacturer_name'] . "</p>
+                            <p>Weight Category: " . $item_rs['weight'] . "GB</p>    
+                            <p>Connection via: " . $item_rs['connection_type'] . "</p>   
                         </div>
                     </div>";
             } else {
